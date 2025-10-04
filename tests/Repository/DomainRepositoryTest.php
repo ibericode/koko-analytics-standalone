ef3cb3bc31a179aadd13cde97bfb11024e1b1a32
Danny van Kooten <hi@dvk.co>
2025-10-04 16:18:24 +0200

    fix domain repository test

--- tests/Repository/DomainRepositoryTest.php
+++ tests/Repository/DomainRepositoryTest.php
@@ -26,12 +26,12 @@ class DomainRepositoryTest extends KernelTestCase
 
         // assert inserting a domain sets the ID
         $domain = new Domain();
-        $domain->setName('website.com');
-        $domain->setTimezone('Europe/Amsterdam');
-        $domain->setPurgeTreshold(100);
-        $domain->setExcludedIpAddresses(['127.0.0.1']);
+        $domain->name = ('website.com');
+        $domain->timezone = ('Europe/Amsterdam');
+        $domain->purge_treshold = (100);
+        $domain->excluded_ip_addresses = (['127.0.0.1']);
         $repo->insert($domain);
-        self::assertGreaterThan(0, $domain->getId());
+        self::assertGreaterThan(0, $domain->id);
 
         // assert repository contains 1 item now
         self::assertCount(1, $repo->getAll());
@@ -39,10 +39,10 @@ class DomainRepositoryTest extends KernelTestCase
         // asert item matches what we just inserted
         $saved = $repo->getByName('website.com');
         self::assertNotNull($saved);
-        self::assertEquals($domain->getName(), $saved->getName());
-        self::assertEquals($domain->getTimezone(), $saved->getTimezone());
-        self::assertEquals($domain->getPurgeTreshold(), $saved->getPurgeTreshold());
-        self::assertEquals($domain->getExcludedIpAddresses(), $saved->getExcludedIpAddresses());
+        self::assertEquals($domain->name, $saved->name);
+        self::assertEquals($domain->timezone, $saved->timezone);
+        self::assertEquals($domain->purge_treshold, $saved->purge_treshold);
+        self::assertEquals($domain->excluded_ip_addresses, $saved->excluded_ip_addresses);
 
         // assert repository is empty again after calling reset
         $repo->reset();
